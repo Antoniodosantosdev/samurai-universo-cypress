7a2a859f9c1f3e2dd70f3abee01d6016e2527bd7
Antonio <ti.antoniocarllos@gmail.com>
2025-05-22 01:05:55 -0300

    configurações do banco de dados no arquivos cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000/",
 
-    viewportWidth: 1440, // Largura da tela (em pixels)
-    viewportHeight: 900, // Altura da tela (em pixels)
+    viewportWidth: 1440, 
+    viewportHeight: 900, 
 
     setupNodeEvents(on, config) {
       const pool = new Pool({
